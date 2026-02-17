2584717ab2595f15a4a8372f241e4c528fb81a23
michal-hynek <hynek.michal@gmail.com>
2026-02-17 07:38:35 -0800

    Basic project structure

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+use clap::Parser;
+
+use crate::runner::{run_file, run_prompt};
+
+mod runner;
+
+/// Rust implementation of Lox interpreter
+#[derive(Parser, Debug)]
+struct CliArgs {
+    /// Source code file path 
+    #[arg(short, long)]
+    file_path: Option<String>,
+}
+
+fn main() -> Result<()> {
+    let cli = CliArgs::parse();
+
+    match cli.file_path {
+        Some(file_path) => run_file(&file_path),
+        None => run_prompt(),
+    }
+}
---
+++ src/runner.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+
+pub fn run_file(file_path: &str) -> Result<()> {
+    todo!("implement run_file");
+}
+
+pub fn run_prompt() -> Result<()> {
+    todo!("implement run_prompt");
+}
+
+fn run(source: &str) -> Result<()> {
+    todo!("implement run");
+}
