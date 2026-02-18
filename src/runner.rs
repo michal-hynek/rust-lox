c24ec43caef790e73675c1758bcd92cc529bf82d
michal-hynek <hynek.michal@gmail.com>
2026-02-18 06:58:42 -0800

    Basic runner implementation

--- src/runner.rs
+++ src/runner.rs
@@ -1,11 +1,35 @@
+use std::{fs::read_to_string, io::{self, Write}};
+
 use anyhow::Result;
 
 pub fn run_file(file_path: &str) -> Result<()> {
-    todo!("implement run_file");
+    let source = read_to_string(file_path)?;
+    run(&source)
 }
 
 pub fn run_prompt() -> Result<()> {
-    todo!("implement run_prompt");
+    let mut input = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+
+        match io::stdin().read_line(&mut input) {
+            Ok(len) => {
+                if len == 0 {
+                    break;
+                }
+            },
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                continue;
+            },
+        }
+
+        run(&input)?;
+    }
+
+    Ok(())
 }
 
 fn run(source: &str) -> Result<()> {
