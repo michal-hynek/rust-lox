5e561029c78ff5993a817d743bf0b10018d50da4
michal-hynek <hynek.michal@gmail.com>
2026-02-20 08:40:07 -0800

    Added TokenType enum

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,7 +1,29 @@
 use anyhow::Result;
 
 #[derive(Debug)]
-pub struct Token;
+pub struct Token {
+    token_type: TokenType,
+}
+
+#[derive(Debug)]
+pub enum TokenType {
+    // single character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+}
 
 pub struct Scanner {
     source: String,
