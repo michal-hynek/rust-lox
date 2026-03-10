5241f42f9bb1169881bbd71846b38d967b471e48
michal-hynek <hynek.michal@gmail.com>
2026-03-10 08:38:13 -0700

    Parser skeleton

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,21 @@
+use crate::{ast::Expr, scanner::Token};
+
 mod ast_printer;
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self {
+            tokens,
+            current: 0,
+        }
+    }
+
+    pub fn parse(&mut self) -> Expr {
+        todo!()
+    }
+}
