b8187293b2871ddee29819ad687b3621bc8952a4
michal-hynek <hynek.michal@gmail.com>
2026-03-22 08:49:44 -0700

    Interpreter - LiteralExpr

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -12,10 +12,75 @@ impl Visitor<LiteralValue> for Intepreter {
     }
 
     fn visit_literal(&self, literal: &LiteralExpr) -> LiteralValue {
-        todo!()
+        literal.value.clone()
     }
 
     fn visit_unary(&self, unary: &UnaryExpr) -> LiteralValue {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test_interpreter {
+    use super::*;
+
+    #[test]
+    fn test_visit_literal_string() {
+        let literal_expr = LiteralExpr {
+            value: LiteralValue::String("foo".to_string()),
+        };
+        let interpreter = Intepreter {};
+
+        let val = interpreter.visit_literal(&literal_expr);
+
+        assert_eq!(LiteralValue::String("foo".to_string()), val);
+    }
+
+    #[test]
+    fn test_visit_literal_number() {
+        let literal_expr = LiteralExpr {
+            value: LiteralValue::Number(123f64),
+        };
+        let interpreter = Intepreter {};
+
+        let val = interpreter.visit_literal(&literal_expr);
+
+        assert_eq!(LiteralValue::Number(123f64), val);
+    }
+
+    #[test]
+    fn test_visit_literal_true() {
+        let literal_expr = LiteralExpr {
+            value: LiteralValue::True,
+        };
+        let interpreter = Intepreter {};
+
+        let val = interpreter.visit_literal(&literal_expr);
+
+        assert_eq!(LiteralValue::True, val);
+    }
+
+    #[test]
+    fn test_visit_literal_false() {
+        let literal_expr = LiteralExpr {
+            value: LiteralValue::False,
+        };
+        let interpreter = Intepreter {};
+
+        let val = interpreter.visit_literal(&literal_expr);
+
+        assert_eq!(LiteralValue::False, val);
+    }
+
+    #[test]
+    fn test_visit_literal_nil() {
+        let literal_expr = LiteralExpr {
+            value: LiteralValue::Nil,
+        };
+        let interpreter = Intepreter {};
+
+        let val = interpreter.visit_literal(&literal_expr);
+
+        assert_eq!(LiteralValue::Nil, val);
     }
 }
